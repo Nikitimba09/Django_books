73bb2d274ef1be3027e1b3057ebebb4694afcaf8
Nikita <nickit.schatalow@yandex.ru>
2022-12-06 01:14:41 +0400

    upd

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Главная страница сайта Мир книг!")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status_exact=2).count()
+
+    num_authors = Author.objects.count()
+
+    return render(request, 'index.html',
+                  context={'num_books': num_books,
+                           'num_instances': num_instances,
+                           'num_instances_available': num_instances_available,
+                           'num_authors': num_authors,
+                           'num_visits': num_visits},
+                  )
+
+
+
+
